cbc8e426804729b9b5576fd1d42906d3598aa3ee
Khine <khinezar70122107@gmail.com>
2023-02-16 11:38:56 -1200

    final

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,32 @@
 require('dotenv').config()
 const express = require('express')
+const mongoose = require('mongoose')
+mongoose.set("strictQuery", false);
+
+const workoutRoutes = require('./routes/workout.js')
 
 //express app
 const app = express()
 
 //middleware
-app.use((req,res,next)=>{
-  console.log(req.path,req.method)
-  next()
-})
+// app.use((req,res,next)=>{
+//   console.log(req.path,req.method)
+//   next()
+// })
+app.use(express.json())
 
 //routes
-app.get('/',(req,res)=>{
-  res.json({mes:'Welcome to the app'})
+app.use("/api/workouts", workoutRoutes);
+
+//connect to db
+mongoose.connect(process.env.MONG_URL)
+.then(()=>{
+  //listen for requests 
+  app.listen(process.env.PORT,() => {
+    console.log('listening on port',process.env.PORT)
+  })
+})
+.catch((error)=>{
+  console.log(error)
 })
 
-//listen for requests 
-app.listen(process.env.PORT,() => {
-    console.log('listening on port',process.env.PORT)
-})
